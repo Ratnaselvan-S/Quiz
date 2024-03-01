377326f33a3df019d96289d6a50b740d09fea0a7
Ratna_Selvan__Tech <110810922+daubermantech@users.noreply.github.com>
2024-03-01 18:04:23 +0530

    Update student-password-reset.php

--- student-password-reset.php
+++ student-password-reset.php
@@ -1,3 +1,16 @@
 <?php
-echo "suganya";
+
+$email = $_POST["email"];
+
+$token = bin2hex(random_bytes(16));
+$token_hash = hash("sha256", $token);
+$expiry = date("Y-m-d H:i:s", time() + 60 * 30);
+
+
+$host = "localhost";
+$dbname = "u475858870_quiz";
+$username = "u475858870_root";
+$dbPassword = "Kalasalingam@339";
+print_r($email);die;
+
 ?>
