59db30050b8a1cc8119f13a832a5536f328c2f7d
Ratna_Selvan__Tech <110810922+daubermantech@users.noreply.github.com>
2024-03-01 18:06:11 +0530

    Update student-password-reset.php

--- student-password-reset.php
+++ student-password-reset.php
@@ -11,6 +11,45 @@ $host = "localhost";
 $dbname = "u475858870_quiz";
 $username = "u475858870_root";
 $dbPassword = "Kalasalingam@339";
-print_r($email);die;
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $dbPassword);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "UPDATE student_db
+            SET reset_token_hash = :token_hash,
+                reset_token_expires_at = :expiry
+            WHERE email = :email";
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->bindValue(':token_hash', $token_hash);
+    $stmt->bindValue(':expiry', $expiry);
+    $stmt->bindValue(':email', $email);
+
+    $stmt->execute();
+
+    // if ($stmt->rowCount()) {
+    //     $mail = require __DIR__ . "/mailer.php";
+
+    //     $mail->setFrom("noreply@example.com");
+    //     $mail->addAddress($email);
+    //     $mail->Subject = "Password Reset";
+    //     // $mail->Body = <<<END
+    //     // Click <a href="https://kare-quiz.alphadevsx.com/student-reset-password.php?token=$token">here</a> 
+    //     // to reset your password.
+    //     // END;
+    //     $mail->Body = '<p><a href="https://kare-quiz.alphadevsx.com/student-reset-password.php?token=$token">here</a>to reset your password.</p>';
+    //     try {
+    //         $mail->send();
+    //     } catch (Exception $e) {
+    //         echo "Message could not be sent. Mailer error: {$mail->ErrorInfo}";
+    //     }
+    // }
+
+    echo "Message sent, please check your inbox.";
+} catch (PDOException $e) {
+    echo "error|Connection failed: " . $e->getMessage();
+}
 
 ?>
